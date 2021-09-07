05998c407319e06d4886d42d43f5cfc26ebcd4de
Emanuele <emanueletonello@gmail.com>
2021-09-07 12:53:02 +1000

    restored normal folder structure

--- generate.js
+++ generate.js
@@ -1,117 +1,89 @@
-const fse = require("fs-extra");
-const jimp = require("jimp");
-const glob = require("glob");
-const path = require("path");
+const jimp = require('jimp')
+const glob = require('glob')
+const path = require('path')
 
 function ensureTrailingSlash(path) {
-  return path.endsWith("/") === false ? path + "/" : path;
+  return path.endsWith('/') === false ? path + '/' : path
 }
 
 function resizeAndSave(factor, source, target) {
-  jimp.read(source, function(err, image) {
-    image.resize(image.bitmap.width * factor, jimp.AUTO);
-    image.write(target, err => {
+  jimp.read(source, function (err, image) {
+    image.resize(image.bitmap.width * factor, jimp.AUTO)
+    image.write(target, (err) => {
       if (err) {
-        console.error(target + " ERROR: " + error);
+        console.error(target + ' ERROR: ' + err)
       } else {
-        console.log(target + " generated");
+        console.log(target + ' generated')
       }
-    });
-  });
+    })
+  })
 }
 
 function generate(files, targetAndroid, targetIos) {
   for (let file of files) {
-    let filename = path.basename(file);
-    let filetype = filename.substr(filename.lastIndexOf("."), filename.length);
+    let filename = path.basename(file)
+    let filetype = filename.substr(filename.lastIndexOf('.'), filename.length)
 
-    filename = filename.substr(0, filename.lastIndexOf("."));
-    if (filename.indexOf("@3x") !== -1) {
-      filename = filename.substr(0, filename.lastIndexOf("@"));
+    filename = filename.substr(0, filename.lastIndexOf('.'))
+    if (filename.indexOf('@3x') !== -1) {
+      filename = filename.substr(0, filename.lastIndexOf('@'))
     }
 
-    console.info("process " + filename + filetype);
+    console.info('process ' + filename + filetype)
 
-    if (typeof targetAndroid !== "undefined") {
+    if (typeof targetAndroid !== 'undefined') {
       resizeAndSave(
-        0.5,
+        0.75 / 4,
         file,
-        `${filename}/` +
-          targetAndroid +
-          filename.toLowerCase() +
-          "-hdpi" +
-          filetype
-      );
+        targetAndroid + 'drawable-ldpi/' + filename.toLowerCase() + filetype
+      )
       resizeAndSave(
-        0.25,
+        1 / 4,
         file,
-        `${filename}/` +
-          targetAndroid +
-          filename.toLowerCase() +
-          "-ldpi" +
-          filetype
-      );
+        targetAndroid + 'drawable-mdpi/' + filename.toLowerCase() + filetype
+      )
       resizeAndSave(
-        1 / 3,
+        1.5 / 4,
         file,
-        `${filename}/` +
-          targetAndroid +
-          filename.toLowerCase() +
-          "-mdpi" +
-          filetype
-      );
+        targetAndroid + 'drawable-hdpi/' + filename.toLowerCase() + filetype
+      )
       resizeAndSave(
-        2 / 3,
+        2 / 4,
         file,
-        `${filename}/` +
-          targetAndroid +
-          filename.toLowerCase() +
-          "-xhdpi" +
-          filetype
-      );
+        targetAndroid + 'drawable-xhdpi/' + filename.toLowerCase() + filetype
+      )
+      resizeAndSave(
+        3 / 4,
+        file,
+        targetAndroid + 'drawable-xxhdpi/' + filename.toLowerCase() + filetype
+      )
       resizeAndSave(
         1,
         file,
-        `${filename}/` +
-          targetAndroid +
-          filename.toLowerCase() +
-          "-xxhdpi" +
-          filetype
-      );
+        targetAndroid + 'drawable-xxxhdpi/' + filename.toLowerCase() + filetype
+      )
     }
 
-    if (typeof targetIos !== "undefined") {
-      resizeAndSave(
-        1 / 3,
-        file,
-        `${filename}/` + targetIos + filename + filetype
-      );
-      resizeAndSave(
-        2 / 3,
-        file,
-        `${filename}/` + targetIos + filename + "@2x" + filetype
-      );
-      resizeAndSave(
-        1,
-        file,
-        `${filename}/` + targetIos + filename + "@3x" + filetype
-      );
+    if (typeof targetIos !== 'undefined') {
+      resizeAndSave(1 / 3, file, targetIos + filename + filetype)
+      resizeAndSave(2 / 3, file, targetIos + filename + '@2x' + filetype)
+      resizeAndSave(1, file, targetIos + filename + '@3x' + filetype)
     }
   }
 }
 
 module.exports = (q, targetAndroid, targetIos) => {
-  if (typeof targetAndroid !== "undefined") {
-    targetAndroid = ensureTrailingSlash(targetAndroid);
+  if (typeof targetAndroid !== 'undefined') {
+    targetAndroid = ensureTrailingSlash(targetAndroid)
   }
 
-  if (typeof targetIos !== "undefined") {
-    targetIos = ensureTrailingSlash(targetIos);
+  if (typeof targetIos !== 'undefined') {
+    targetIos = ensureTrailingSlash(targetIos)
   }
 
-  if (q.length == 1 && q[0].indexOf("*") !== -1) {
-    glob(q[0], {}, (er, files) => generate(files, targetAndroid, targetIos));
+  if (q.length == 1 && q[0].indexOf('*') !== -1) {
+    glob(q[0], {}, (er, files) => generate(files, targetAndroid, targetIos))
   } else {
-    generate(q, targetAndroid, targetIos);
+    generate(q, targetAndroid, targetIos)
   }
-};
+}
